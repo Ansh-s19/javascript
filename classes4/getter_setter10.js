b1f4b719b6248bac5809749593682511d2cd56d7
Ansh-s21 <anshit985sharma@gmail.com>
2025-04-01 15:01:59 +0530

    changes

--- classes4/getter_setter10.js
+++ classes4/getter_setter10.js
@@ -12,7 +12,45 @@ class user{
         this._password = newPassword;
     }
 
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(newemail){
+        this._email=newemail
+    }
+    //They enable encapsulation, ensuring data integrity by providing methods to read and write property values instead of directly accessing them.
+
 
 }
-const a=new user("ansh@gmail.com","12345678")
+const a=new user("ansh@gmail.com","abc")
 console.log(a.password);
+console.log(a.email);
+console.log(a._email);
+
+//old way 
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const chai = new User("chai@chai.com", "chai")
+
+console.log(chai.email);
